0af289880d49dccba10762084089c491a2fc1c6c
KRISHNA BHORE <81962336+krishnabhore@users.noreply.github.com>
2025-06-02 19:45:07 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "azs" {
 
 variable "public_subnet_cidrs" {
  type        = list(string)
- description = "Public Subnet CIDR values"
+ description = "Public Subnet CdIDR values"
  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
  
